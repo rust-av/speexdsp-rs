4c03f82291985cc786982364daca78b0edf5fe53
apiraino <apiraino@users.noreply.github.com>
2018-05-30 14:47:27 +0200

    revert lib.rs (remove tests)
    Signed-off-by: apiraino <apiraino@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,9 @@ pub mod resampler;
 
 #[cfg(test)]
 mod tests {
-    use resampler::State;
-    const RATE: usize = 48000;
-
     #[test]
-    fn test_quality() {
-        let st = State::new(1, RATE, RATE, 4).unwrap();
-        st.set_quality(10).unwrap();
-        assert_eq!(10, st.get_quality());
-    }
-
-    #[test]
-    fn test_rate() {
-        let mut st = State::new(1, RATE, RATE, 4).unwrap();
-        let expected_in_rate = 44100;
-        let expected_out_rate = 44000;
-        st.set_rate(expected_in_rate, expected_out_rate);
-        assert_eq!((expected_in_rate, expected_out_rate), st.get_rate());
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 
 }
