00db2962374944e8a6860b174165f796df13747c
Luni-4 <luni-4@hotmail.it>
2019-06-21 16:03:45 +0200

    Add testdenoise test to the examples

---
+++ examples/testdenoise.rs
@@ -0,0 +1,37 @@
+#[cfg(feature = "sys")]
+extern crate byteorder;
+extern crate speexdsp;
+
+#[cfg(feature = "sys")]
+fn main() {
+    use byteorder::{BigEndian, ByteOrder};
+    use speexdsp::preprocess::SpeexPreprocessConst::*;
+    use speexdsp::preprocess::*;
+    use std::io::Read;
+
+    const NN: usize = 160;
+    let mut input: [i16; NN] = [0; NN];
+    let mut buffer: [u8; NN * 2] = [0; NN * 2];
+
+    let mut st = SpeexPreprocess::new(NN, 8000).unwrap();
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DENOISE, 1f32);
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_AGC, 0f32);
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_AGC_LEVEL, 8000f32);
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB, 0f32);
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_DECAY, 0f32);
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_LEVEL, 0f32);
+
+    while let Ok(n) = std::io::stdin().read(&mut buffer) {
+        if n == 0 {
+            break;
+        }
+        BigEndian::read_i16_into(&buffer, &mut input);
+        st.preprocess_run(&mut input);
+        println!("{:?}", &input[..]);
+    }
+}
+
+#[cfg(not(feature = "sys"))]
+fn main() {
+    unimplemented!();
+}
