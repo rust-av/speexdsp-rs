166d717c097c85b4fceac1156fc7c62873e614a1
Luni-4 <luni-4@hotmail.it>
2019-06-25 18:34:06 +0200

    Refactor testdenoise a little

--- examples/testdenoise.rs
+++ examples/testdenoise.rs
@@ -1,13 +1,15 @@
-#[cfg(feature = "sys")]
+#![allow(dead_code)]
+#![allow(unused_imports)]
 extern crate byteorder;
 extern crate speexdsp;
 
+use byteorder::{BigEndian, ByteOrder};
+use std::io::Read;
+
 #[cfg(feature = "sys")]
 fn main() {
-    use byteorder::{BigEndian, ByteOrder};
     use speexdsp::preprocess::SpeexPreprocessConst::*;
     use speexdsp::preprocess::*;
-    use std::io::Read;
 
     const NN: usize = 160;
     let mut input: [i16; NN] = [0; NN];
@@ -16,10 +18,13 @@ fn main() {
     let mut st = SpeexPreprocess::new(NN, 8000).unwrap();
     st.preprocess_ctl(SPEEX_PREPROCESS_SET_DENOISE, 1).unwrap();
     st.preprocess_ctl(SPEEX_PREPROCESS_SET_AGC, 0).unwrap();
-    st.preprocess_ctl(SPEEX_PREPROCESS_SET_AGC_LEVEL, 8000).unwrap();
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_AGC_LEVEL, 8000)
+        .unwrap();
     st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB, 0).unwrap();
-    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_DECAY, 0f32).unwrap();
-    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_LEVEL, 0f32).unwrap();
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_DECAY, 0f32)
+        .unwrap();
+    st.preprocess_ctl(SPEEX_PREPROCESS_SET_DEREVERB_LEVEL, 0f32)
+        .unwrap();
 
     while let Ok(n) = std::io::stdin().read(&mut buffer) {
         if n == 0 {
