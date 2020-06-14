9703d6c27acf0b3c5c761ef54b093d32ad2a2cc1
Shamir Khodzha <khodzha.sh@gmail.com>
2020-06-15 01:25:20 +0300

    added dynnative feature
    it selects proper avx/sse3/native feature at run time
    dynnative wrappers added because rustc forbids using inline(always) and
    target_feature together

---
+++ resampler/src/speex/dynnative/sse3_wrapper.rs
@@ -0,0 +1,65 @@
+#[allow(clippy::too_many_arguments)]
+#[target_feature(enable = "sse3")]
+pub unsafe fn interpolate_step_single(
+    in_slice: &[f32],
+    out_slice: &mut [f32],
+    out_stride: usize,
+    out_sample: usize,
+    oversample: usize,
+    offset: usize,
+    n: usize,
+    sinc_table: &[f32],
+    frac: f32,
+) {
+    crate::speex::sse3::interpolate_step_single(
+        in_slice, out_slice, out_stride, out_sample, oversample, offset, n,
+        sinc_table, frac,
+    );
+}
+
+#[allow(clippy::too_many_arguments)]
+#[target_feature(enable = "sse3")]
+pub unsafe fn interpolate_step_double(
+    in_slice: &[f32],
+    out_slice: &mut [f32],
+    out_stride: usize,
+    out_sample: usize,
+    oversample: usize,
+    offset: usize,
+    n: usize,
+    sinc_table: &[f32],
+    frac: f32,
+) {
+    crate::speex::sse3::interpolate_step_double(
+        in_slice, out_slice, out_stride, out_sample, oversample, offset, n,
+        sinc_table, frac,
+    );
+}
+
+#[target_feature(enable = "sse3")]
+pub unsafe fn direct_step_single(
+    in_slice: &[f32],
+    out_slice: &mut [f32],
+    out_stride: usize,
+    out_sample: usize,
+    n: usize,
+    sinc_table: &[f32],
+) {
+    crate::speex::sse3::direct_step_single(
+        in_slice, out_slice, out_stride, out_sample, n, sinc_table,
+    );
+}
+
+#[target_feature(enable = "sse3")]
+pub unsafe fn direct_step_double(
+    in_slice: &[f32],
+    out_slice: &mut [f32],
+    out_stride: usize,
+    out_sample: usize,
+    n: usize,
+    sinc_table: &[f32],
+) {
+    crate::speex::sse3::direct_step_double(
+        in_slice, out_slice, out_stride, out_sample, n, sinc_table,
+    );
+}
